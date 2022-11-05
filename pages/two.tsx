349fa6bf725bda1cdacecb64e2387b0a6f03b7f6
Chris Hamilton <chris_h_27@hotmail.ca>
2022-11-05 16:46:05 -0400

    add text

--- pages/two.tsx
+++ pages/two.tsx
@@ -6,6 +6,7 @@ export default function TwoPage() {
     <>
       <h1>Component Two</h1>
       <p>Go forward a few times, then back</p>
+      <p>Notice how the url changes when the component does not</p>
       <button onClick={() => history.back()}>BACK</button>
       <button onClick={() => Router.push("/one")}>FORWARD</button>
     </>
